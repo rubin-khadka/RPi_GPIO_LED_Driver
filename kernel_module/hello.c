4cd08d6f1b2039c9fb1ef5c5cbde59d085a53423
rubin-khadka <rubin.robotic@gmail.com>
2026-05-27 20:06:21 +0200

    added hello kernel module and makefile

---
+++ kernel_module/hello.c
@@ -0,0 +1,25 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("RUBIN KHADKA");
+MODULE_DESCRIPTION("A simple HELLO linux kernel module.");
+
+static char *myname = "World";
+module_param(myname, charp, S_IRUGO);
+MODULE_PARM_DESC(myname, "A name to say hello to");
+
+static int __init hello_init(void) 
+{
+    printk(KERN_INFO "Hello, %s!\n", myname);
+    return 0;
+}
+
+static void __exit hello_exit(void)
+{
+    printk(KERN_INFO "Goodbye, %s!\n", myname);
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
